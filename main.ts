e9e22831682d426f885fa06b7036b0229a467944
Thamzheneee <114672083+Thamzheneee@users.noreply.github.com>
2022-09-29 21:25:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+if (true) {
+    basic.showArrow(ArrowNames.East)
+    basic.showString("Today i want sing a song")
+    led.setBrightness(255)
+    basic.showIcon(IconNames.Asleep)
+    music.playMelody("C D E F G A B C5 ", 55)
+    basic.showIcon(IconNames.Happy)
+    basic.showString("Thank You ")
+    basic.showArrow(ArrowNames.West)
+    basic.showString("Bye Bye 👋 ")
+}
